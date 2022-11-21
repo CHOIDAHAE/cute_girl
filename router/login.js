05583ba981aff3f0ef6b70ac263340a91c851c9a
CHOIDAHAE <otae5721@naver.com>
2022-11-21 17:31:04 +0900

    캡차 이미지 개발 완료, 실제 비밀번호 수정 로직 남음

--- router/login.js
+++ router/login.js
@@ -180,7 +180,7 @@ module.exports = function(app){
 		url: api_url,
 			headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
 		};
-		var writeStream = fs.createWriteStream('./img/captcha.jpg');
+		var writeStream = fs.createWriteStream('./public/images/captcha.jpg');
 		var _req = request.get(options).on('response', function(response) {
 			//console.log(response); // 200
             //console.log(response.statusCode) // 200
@@ -188,5 +188,26 @@ module.exports = function(app){
 		});
 		_req.pipe(writeStream); // file로 출력
 		_req.pipe(res); // 브라우저로 출력
+	});
+
+    // 사용자 입력값 검증 요청
+    app.get('/captcha/result', function (req, res) {
+		var code = "1";
+        console.log(req.query);
+		var api_url = 'https://openapi.naver.com/v1/captcha/nkey?code=' + code + '&key=' + req.query.key + '&value=' + req.query.value;
+		var request = require('request');
+		var options = {
+			url: api_url,
+			headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+		};
+		request.get(options, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+				res.end(body);
+			} else {
+				res.status(response.statusCode).end();
+				console.log('error = ' + response.statusCode);
+			}
+		});
 	});
 }
