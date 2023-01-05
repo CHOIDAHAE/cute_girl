7ce35a27080641805bbd6ff9a6bc5d1f33f38742
CHOIDAHAE <otae5721@naver.com>
2023-01-05 18:27:25 +0900

    모임나가기(개발중)

--- router/group.js
+++ router/group.js
@@ -11,6 +11,11 @@ var mybatisMapper = require('mybatis-mapper');
 mybatisMapper.createMapper( ['./mapper/GroupDAO_SQL.xml']);
 
 module.exports = function(app){
+	// 그룹화면
+	app.get('/group', function(req, res, next){
+        res.render('./group/group');
+    })
+
   	// 새 모임 추가하기
 	app.post("/insertNewGroup", function(req, res){		
 		oracledb.getConnection({
@@ -100,8 +105,7 @@ module.exports = function(app){
 
 			//내 그룹 조회
 			let selectMyGroup = mybatisMapper.getStatement('GroupDAO','selectMyGroup', {"emplyrSn":req.body.emplyrSn}, format);
-			
-			console.log(selectMyGroup);
+
 			//쿼리문 실행
 			conn.execute(selectMyGroup, function(err,result){
 				if(err){
@@ -109,6 +113,7 @@ module.exports = function(app){
 					res.json({"Status":"F"});
 					return;
 				}
+				
 				res.json(result.rows);
 				
 				doRelease(conn);
@@ -116,9 +121,50 @@ module.exports = function(app){
 		});
 	})
 
-	app.get('/group', function(req, res, next){
-        res.render('./group/group');
-    })
+	// 내 모임 조회하기
+	app.post("/selectedGouprSn", function(req, res, next){
+		oracledb.getConnection({
+			user:dbConfig.user,
+			password:dbConfig.password,
+			connectString:dbConfig.connectString,
+			externalAuth  : dbConfig.externalAuth
+		},function(err,con){
+			if(err){
+				console.log("Oracle Connection failed(selectMyGroup)",err);
+			} else {
+				//console.log("Oracle Connection success(selectMyGroup)");
+			}
+			conn = con;
+
+			//query format
+			let format = {language: 'sql', indent: ''};
+			console.log(req.body);
+			console.log(req.body.emplyrSn);
+			console.log(req.body.num);
+
+			var param = {
+				"emplyrSn"	: req.body.emplyrSn,
+				"num"		: req.body.num
+			}
+
+			//내 그룹 조회
+			let selectedGouprSn = mybatisMapper.getStatement('GroupDAO','selectedGouprSn', param, format);
+
+			//쿼리문 실행
+			conn.execute(selectedGouprSn, function(err,result){
+				if(err){
+					console.log("selectMyGroup failed :", err);
+					res.json({"Status":"F"});
+					return;
+				}
+				
+				var groupSn = result.rows[0][0];
+				// 요거 update해야함.
+
+				doRelease(conn);
+			});
+		});
+	})
 
 	function doRelease(conn){
 		conn.close(function(err){
