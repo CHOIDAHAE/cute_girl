28c20d25fce12534aa20aaf85030f2761bdd7b93
emeng3882 <emeng3882@gmail.com>
2023-01-26 17:57:54 +0900

    마이페이지 수정

--- router/myPage.js
+++ router/myPage.js
@@ -49,6 +49,54 @@ module.exports = function(app){
 			});  
 		});
 
+		//프로필 저장하기
+		app.post('/saveProfile', function(req, res){
+			console.log("saveProfile");
+			
+			var sEmplyrSn = req.body.sEmplyrSn;
+			var emplyrNm = req.body.emplyrNm;
+			var emplyrId = req.body.emplyrId;
+			var emailAdres = req.body.emailAdres;
+	
+			
+			oracledb.getConnection({
+				user:dbConfig.user,
+				password:dbConfig.password,
+				connectString:dbConfig.connectString,
+				externalAuth  : dbConfig.externalAuth
+			},function(err,con){
+				if(err){
+					console.log("Oracle Connection failed(/saveProfile)",err);
+				} else {
+					console.log("Oracle Connection success(/saveProfile)");
+				}
+				conn = con;
+					
+				//query format
+				let format = {language: 'sql', indent: ''};
+				
+				var param = {
+					sEmplyrSn : sEmplyrSn,
+					emplyrNm : emplyrNm,
+					emplyrId : emplyrId,
+					emailAdres : emailAdres
+				}
+	
+				let query = mybatisMapper.getStatement('MyPageDAO','updateMyProfile', param, format);
+				conn.execute(query, function(err,result){
+					console.log("MyPageDAO.updateMyProfile");
+					console.log(query);
+					if(err){
+						console.log(err);
+						res.json("F");
+					}
+					conn.commit();
+				});
+				res.json({"emplyrSn":req.session.user.emplyrSn});
+				// doRelease(conn);
+			});
+		});
+
 		function doRelease(conn){
 			conn.close(function(err){
 				if(err){
